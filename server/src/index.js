22bedf58d4cbbdd67a67659d0221ac3a659a54de
Jermaine Riley M. Pasion <jmpasion@student.hau.edu.ph>
2025-11-19 11:19:21 +0800

    Add: server setup connected to postgresql

---
+++ server/src/index.js
@@ -0,0 +1,38 @@
+import express from "express"
+import cors from "cors" 
+import dotenv from "dotenv"
+import pool from "./db.js";
+import errorHandling from "./middlewares/errorHandler.js";
+
+dotenv.config();
+
+const app = express();
+const port =  process.env.PORT || 3001;
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+//routes
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+//use for testing database connection
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({ message: "Database connected!", time: result.rows[0] });
+  } catch (err) {
+    console.error("DB Error:", err);
+    res.status(500).json({ error: "Database connection failed" });
+  }
+});
+
+//error handling middleware
+app.use(errorHandling);
+
+//server run
+app.listen(port, ()=> {
+    console.log(`Server is running on http://localhost:${port}`)
+})
