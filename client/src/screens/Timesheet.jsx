1c884ba15e8ca0ebf657429bc647a5d3f946ae75
donotlook <repodsalgo200zzz@gmail.com>
2025-11-20 17:17:07 +0800

    add: date range + sidebar wrap in timesheet

--- client/src/screens/Timesheet.jsx
+++ client/src/screens/Timesheet.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import AttendanceTable from '../components/AttendanceTable';
+import DashboardLayout from "../components/DashboardLayout/DashboardLayout";
 import '../styles/AttendanceTable.css'
+import '../styles/DateRange.css'
 
 function Timesheet() {
+
+  const date = new Date();
+  const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+  const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0)
+
   return (
-  <div>
-    <div className='attendance-head'>
-      <h1>Timesheet</h1>
+    <div>
+      <DashboardLayout>
+        <div className="daterange_bar">
+          Date Range: {firstDay.toLocaleDateString()} - {lastDay.toLocaleDateString()} 
+        </div>
+
+        <div className='attendance-head'>
+          <h1>Timesheet</h1>
+        </div>
+
+        <AttendanceTable />
+      </DashboardLayout>
     </div>
-    <AttendanceTable />
-  </div>
   )
 }
 
