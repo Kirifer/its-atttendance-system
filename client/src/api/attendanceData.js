0c836e67f58c9548e1e30f61edac8696ed7c4e2d
donotlook <repodsalgo200zzz@gmail.com>
2025-11-20 17:15:14 +0800

    add: dummy data update

--- client/src/api/attendanceData.js
+++ client/src/api/attendanceData.js
@@ -1,18 +1,32 @@
 export const dummyAttendance = {
   users: [
     {
-      "Intern": "john.doe@example.com",
+      "Intern": "john.doe@email.com",
       "Date": "Nov-20-2025",
       "Time In": "09:00 AM",
       "Time Out": "06:00 PM",
       "TOTAL": "8 hrs"
     },
     {
-      "Intern": "jane.smith@example.com",
+      "Intern": "jane.smith@yahoo.com",
       "Date": "Nov-20-2025",
       "Time In": "10:00 AM",
       "Time Out": "07:00 PM",
       "TOTAL": "9 hrs"
+    },
+    {
+      "Intern": "alice.intern@gmail.com",
+      "Date": "Nov-20-2025",
+      "Time In": "10:00 AM",
+      "Time Out": "07:00 PM",
+      "TOTAL": "9 hrs"
+    },
+    {
+      "Intern": "bob.11172025@outlook.com",
+      "Date": "Nov-20-2025",
+      "Time In": "9:00 AM",
+      "Time Out": "06:00 PM",
+      "TOTAL": "8 hrs"
     }
   ]
 };
