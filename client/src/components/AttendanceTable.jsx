215f77a01890d29dfa1353063b5e16c75feee8a8
C19rk <clarkmanuntag@gmail.com>
2025-11-24 14:18:38 +0800

    Added User Dropdown Menu

--- client/src/components/AttendanceTable.jsx
+++ client/src/components/AttendanceTable.jsx
@@ -1,37 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import '../styles/AttendanceTable.css'
-import { dummyAttendance } from '../api/attendanceData.js'
+import React, { useEffect, useState } from "react";
+import "../styles/AttendanceTable.css";
+import { dummyAttendance } from "../api/attendanceData.js";
 
 export default function AttendanceTable() {
-    const [records, setRecords] = useState([]);
+  const [records, setRecords] = useState([]);
 
-    useEffect(() => {
-        setRecords(dummyAttendance.users);
-    }, []);
+  useEffect(() => {
+    setRecords(dummyAttendance.users);
+  }, []);
 
-    return (
-        <div className="attendance_body">
-            <div className='attendance_container'>
-                <table className='attendance_tbl'>
-                    <thead>
-                        <tr>
-                            {records[0] && Object.keys(records[0]).map((col, i) => (
-                                <th key={i}>{col}</th>
-                            ))}
-                        </tr>
-                    </thead>
+  return (
+    <div className="attendance_body">
+      <div className="attendance_container">
+        <table className="attendance_tbl">
+          <thead>
+            <tr>
+              {records[0] &&
+                Object.keys(records[0]).map((col, i) => <th key={i}>{col}</th>)}
+            </tr>
+          </thead>
 
-                    <tbody>
-                        {records.map((record, i) => (
-                            <tr key={i}>
-                                {Object.entries(record).map(([colName, val], j) => (
-                                    <td key={j} data-label={colName}>{val}</td>
-                                ))}
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    )
+          <tbody>
+            {records.map((record, i) => (
+              <tr key={i}>
+                {Object.entries(record).map(([colName, val], j) => (
+                  <td key={j} data-label={colName}>
+                    {val}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
