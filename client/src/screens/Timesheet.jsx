d4ad781ccd4d3fe8db2b888950bec460f32d1f0f
C19rk <clarkmanuntag@gmail.com>
2025-11-24 09:28:35 +0800

    Fixed Timesheet.jsx import for DashboardLayout.jsx

--- client/src/screens/Timesheet.jsx
+++ client/src/screens/Timesheet.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import AttendanceTable from '../components/AttendanceTable';
-import DashboardLayout from "../components/DashboardLayout/DashboardLayout";
-import '../styles/AttendanceTable.css'
-import '../styles/DateRange.css'
+import React from "react";
+import AttendanceTable from "../components/AttendanceTable";
+import DashboardLayout from "../components/DashboardLayout";
+import "../styles/AttendanceTable.css";
+import "../styles/DateRange.css";
 
 function Timesheet() {
-
   const date = new Date();
   const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-  const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0)
+  const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
 
   return (
     <div>
       <DashboardLayout>
         <div className="daterange_bar">
-          Date Range: {firstDay.toLocaleDateString()} - {lastDay.toLocaleDateString()} 
+          Date Range: {firstDay.toLocaleDateString()} -{" "}
+          {lastDay.toLocaleDateString()}
         </div>
 
-        <div className='attendance-head'>
+        <div className="attendance-head">
           <h1>Timesheet</h1>
         </div>
 
         <AttendanceTable />
       </DashboardLayout>
     </div>
-  )
+  );
 }
 
-export default Timesheet
+export default Timesheet;
