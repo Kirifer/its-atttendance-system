aa70d66405f836eac7139c9c3fe9df348e39407d
donotlook <repodsalgo200zzz@gmail.com>
2025-11-25 13:48:13 +0800

    add: prev. & next month btin daterange bar

--- client/src/screens/Timesheet.jsx
+++ client/src/screens/Timesheet.jsx
@@ -1,22 +1,44 @@
-import React from "react";
+import { useState } from "react";
 import AttendanceTable from "../components/AttendanceTable";
 import DashboardLayout from "../components/DashboardLayout";
 import "../styles/AttendanceTable.css";
 import "../styles/DateRange.css";
 
 function Timesheet() {
-  const date = new Date();
-  const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-  const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+
+  const [currentDate, setCurrentDate] = useState(new Date()); 
+
+  // const date = new Date();
+
+  const firstDay = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+  const lastDay = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+
+  const handlePrevMonth = () => {
+    setCurrentDate (
+      new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1)
+    );
+  };
+
+   const handleNextMonth = () => {
+    setCurrentDate (
+      new Date(currentDate.getFullYear(), currentDate.getMonth() +1, 1)
+    )
+   }
 
   return (
     <div>
       <DashboardLayout>
+        <div className="daterange_container">
+          <button className="daterange_btn" onClick={handlePrevMonth}>&larr;</button>
+
         <div className="daterange_bar">
           Date Range: {firstDay.toLocaleDateString()} -{" "}
           {lastDay.toLocaleDateString()}
         </div>
 
+          <button className="daterange_btn" onClick={handleNextMonth}>&rarr;</button>
+        </div>
+    
         <div className="attendance_head">
           <h1>Timesheet</h1>
         </div>
