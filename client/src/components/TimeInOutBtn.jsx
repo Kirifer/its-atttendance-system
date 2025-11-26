92962ae951f1ebda467348f11141880dad313057
donotlook <repodsalgo200zzz@gmail.com>
2025-11-26 13:57:50 +0800

    add: time in/out btn lock

--- client/src/components/TimeInOutBtn.jsx
+++ client/src/components/TimeInOutBtn.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import { useState } from 'react'
 import "../styles/TimeInOut.css"
 function TimeInOut() {
+
+  const [isTimedIn, setIsTimedIn] = useState(false);
+
+  const handleTimeIn = () => {
+    alert("Timed in");
+    setIsTimedIn(true);
+  }
+
+  const handleTimeOut = () => {
+    alert("Timed out");
+    setIsTimedIn(false);
+  }
+
   return (
     <div className="tinout_body">
       <div className="tinout_container">
-        <button className="tinout_in_btn" /*onClick={}*/>Time In</button> 
-        <button className="tinout_out_btn" /*onClick={}*/>Time Out</button>
+        <button id="in" className="tinout_in_btn" onClick={handleTimeIn} disabled={isTimedIn}>Time In</button>
+        <button className="tinout_out_btn" onClick={handleTimeOut} disabled={!isTimedIn}>Time Out</button>
       </div>
     </div>
   )
