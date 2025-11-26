d9419e348023f8570bbbaf83b4ce3a4f5b69fbb2
C19rk <142424578+C19rk@users.noreply.github.com>
2025-11-26 15:22:05 +0800

    Removed unnecessary error message line in ProtectedRoute.jsx

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -5,7 +5,7 @@ function ProtectedRoute({ children, adminOnly = false }) {
   const token = localStorage.getItem("token");
 
   if (!token) {
-    return <ErrorPage message="SIGN IN FIRST TO ACCESS THIS PAGE!!" />;
+    return <ErrorPage message="" />;
   }
 
   if (adminOnly) {
