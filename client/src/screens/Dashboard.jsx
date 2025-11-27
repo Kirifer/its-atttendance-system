30f43d041fb8989230560777c53cb3c581093b8c
Jermaine Riley M. Pasion <jmpasion@student.hau.edu.ph>
2025-11-27 11:56:04 +0800

    Add: Dashboard Live clock and user details design (no live data)

--- client/src/screens/Dashboard.jsx
+++ client/src/screens/Dashboard.jsx
@@ -3,6 +3,8 @@ import { useState } from "react"
 import "../styles/Dashboard.css";
 import TimeInOut from "../components/TimeInOutBtn.jsx"
 import DashboardLayout from "../components/DashboardLayout";
+import Clock from "../components/dashboard/Clock.jsx";
+import LogsCard from "../components/dashboard/LogsCard.jsx";
 
 function Dashboard() {
   const user = JSON.parse(localStorage.getItem("user"));
@@ -10,23 +12,30 @@ function Dashboard() {
 
   const navigate = useNavigate();
 
-  const handleSignOut = () => {
-    localStorage.removeItem("token");
-    navigate("/login");
-  };
-
   return (
     <DashboardLayout>
-      <div>
-        <h1>Dashboard</h1>
-        <div>
-          <button className="return-button" onClick={() => navigate("/")}>
-            Return to home
-          </button>
-          <TimeInOut userId={user.id}
-            onAttendanceChange={() => setReload((r) => !r)} />
+        <div className="dashboard__main">
+          <div>
+            <div className="dashboard__containerone">
+              <div className="dashboard__left">
+                 <Clock format="HH:mm:ss" />
+                <TimeInOut
+                  userId={user.id}
+                  onAttendanceChange={() => setReload((r) => !r)}
+                />
+              </div>
+              <div className="dashboard__right">
+                <LogsCard
+                  userName={user.name}
+                  logs={[
+                    { timeIn: "November 18, 2025 09:00 AM", timeOut: "November 18, 2025 06:00 PM" },
+                    { timeIn: "November 19, 2025 09:05 AM", timeOut: "November 19, 2025 06:10 PM" },
+                  ]}
+                />
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
     </DashboardLayout>
   );
 }
