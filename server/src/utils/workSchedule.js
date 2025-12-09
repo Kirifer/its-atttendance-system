f24293d727b54526de96485031e147e73d3de9bd
donotlook <repodsalgo200zzz@gmail.com>
2025-12-09 10:49:53 +0800

    add: default sched helper

---
+++ server/src/utils/workSchedule.js
@@ -0,0 +1,20 @@
+export const workSchedule = (date = new Date()) => {
+    const day = date.getDay();
+
+    if (day === 0 || day === 0) {
+        return null;
+    }
+    
+    const start = new Date();
+    const end = new Date();
+
+    if (day === 3) {
+        start.setHours(10,0,0,0);
+        end.setHours(19, 0, 0, 0);
+    } else {
+        start.setHours(9, 0, 0, 0);
+        end.setHOurs(6, 0, 0, 0);
+    }
+
+    return { start, end };
+}
