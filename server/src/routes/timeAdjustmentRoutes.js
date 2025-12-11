64532e569c91bd85b910f944cc53e7c478b73646
C19rk <clarkmanuntag@gmail.com>
2025-12-11 13:50:28 +0800

    Updated time adjustment route, model, & controller & schema.prisma to handle file upload.

--- server/src/routes/timeAdjustmentRoutes.js
+++ server/src/routes/timeAdjustmentRoutes.js
@@ -1,5 +1,7 @@
 import express from "express";
 import authMiddleware from "../middlewares/authMiddleware.js";
+import multer from "multer";
+import path from "path";
 
 import {
   fileTimeAdjustment,
@@ -12,7 +14,33 @@ import {
 
 const router = express.Router();
 
-router.post("/", authMiddleware, fileTimeAdjustment);
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({
+  storage,
+  limits: { fileSize: 10 * 1024 * 1024 }, // 10 MB limit
+  fileFilter: (req, file, cb) => {
+    if (!file.mimetype.startsWith("image/")) {
+      return cb(new Error("Only image files are allowed!"), false);
+    }
+    cb(null, true);
+  },
+});
+
+router.post(
+  "/",
+  authMiddleware,
+  upload.single("attachment"),
+  fileTimeAdjustment
+);
 router.get("/", authMiddleware, fetchTimeAdjustments);
 // view user requests
 router.get("/my-requests", authMiddleware, fetchMyTimeAdjustments);
