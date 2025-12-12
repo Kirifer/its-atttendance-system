e14e61e9c14caafc490e400030a4adbc063de097
donotlook <repodsalgo200zzz@gmail.com>
2025-12-12 12:50:08 +0800

    chore: moved AttBtn to Dashboard folder

--- client/src/screens/Dashboard.jsx
+++ client/src/screens/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import "../styles/Dashboard.css";
-import AttBtn from "../components/AttBtn.jsx";
+import AttBtn from "../components/Dashboard/AttBtn.jsx";
 import DashboardLayout from "../components/DashboardLayout";
 import Clock from "../components/Dashboard/Clock.jsx";
 import LogsCard from "../components/Dashboard/LogsCard.jsx";
