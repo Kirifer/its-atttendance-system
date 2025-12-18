4f50d930cc5a2e90f83e9e5ac9e332fc483a7158
donotlook <repodsalgo200zzz@gmail.com>
2025-12-18 14:20:44 +0800

    chore: set custom sched and set intern hours moved to Approvals.jsx

--- client/src/screens/Approvals.jsx
+++ client/src/screens/Approvals.jsx
@@ -6,13 +6,21 @@ import {
   deleteLeave,
 } from "../api/leaveAdmin";
 import LeaveTable from "../components/LeaveTable";
+import "../styles/EditUserSchedule.css";
 import "../styles/Approvals.css";
 import TimeAdjustmentTable from "../components/TimeAdjustmentTable";
 import AdminCRUD from "../components/AdminCRUD";
+import EditUserSchedule from "../components/EditUserSchedule.jsx";
+import useUserSchedule from "../hooks/useUserSchedule.js"
+import SetOjtHours from "../components/SetOjtHours.jsx";
 
 function Approvals() {
+  const user = JSON.parse(localStorage.getItem("user"));
+
   const [leaves, setLeaves] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const userSchedule = useUserSchedule();
 
   const fetchLeaves = async () => {
     try {
@@ -78,6 +86,22 @@ function Approvals() {
           gravida ante, vel egestas turpis.
         </p>
         <AdminCRUD />
+        <h1 className="admin__title">Handle Interns</h1>
+        <p className="admin__description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec leo
+          diam, interdum nec placerat in, venenatis egestas justo. Nam eu
+          gravida ante, vel egestas turpis.
+        </p>
+        <div className="custom_schedule_wrapper">
+          <EditUserSchedule userSchedule={userSchedule} user={user} />
+          <button
+            className="custom_schedule_btn"
+            onClick={userSchedule.openSchedule}
+          >
+            Set Custom Schedule
+          </button>
+          <SetOjtHours />
+        </div>
       </div>
     </DashboardLayout>
   );
--- client/src/screens/Dashboard.jsx
+++ client/src/screens/Dashboard.jsx
@@ -8,15 +8,10 @@ import LogsCard from "../components/Dashboard/LogsCard.jsx";
 import FiledLeavesCard from "../components/Dashboard/FiledLeavesCard.jsx";
 import Calendar from "../components/Dashboard/Calendar.jsx";
 import UserStatusCard from "../components/Dashboard/UserStatusCard.jsx";
-import EditUserSchedule from "../components/EditUserSchedule.jsx";
-import useUserSchedule from "../hooks/useUserSchedule.js"
-import SetOjtHours from "../components/SetOjtHours.jsx";
 
 function Dashboard() {
   const user = JSON.parse(localStorage.getItem("user"));
   const [reload, setReload] = useState(false);
-
-  const userSchedule = useUserSchedule();
 
   const navigate = useNavigate();
 
@@ -27,24 +22,11 @@ function Dashboard() {
           <div className="dashboard__containerone">
             <div className="dashboard__left">
               <Clock format="HH:mm:ss" />
-              {user.role === "ADMIN" ? (
-                <div className="custom_schedule_wrapper">
-                  <EditUserSchedule userSchedule={userSchedule} user={user} />  
-                  <button
-                    className="custom_schedule_btn"
-                    onClick={userSchedule.openSchedule}
-                  >
-                    Set Custom Schedule
-                  </button>
-                  <SetOjtHours />
-                </div>          
-              ) : (
                 <AttBtn
                   userId={user.id}
                   reload={reload}
                   onAttendanceChange={() => setReload((r) => !r)}
                 />
-              )}
             </div>
                 
             <div className="dashboard__right">
