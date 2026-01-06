0e6fecf8e713f8ef9d2d721ccaf3724bf739ed3f
Jermaine Riley M. Pasion <jmpasion@student.hau.edu.ph>
2026-01-06 16:05:37 +0800

    chore: timeAdjustmentRoutes code cleanup

--- server/src/routes/timeAdjustmentRoutes.js
+++ server/src/routes/timeAdjustmentRoutes.js
@@ -6,7 +6,6 @@ import path from "path";
 import {
   fileTimeAdjustment,
   fetchTimeAdjustments,
-  // view user requests
   fetchMyTimeAdjustments,
   updateTimeAdjustmentStatus,
   deleteTimeAdjustment,
@@ -42,11 +41,8 @@ router.post(
   fileTimeAdjustment
 );
 router.get("/", authMiddleware, fetchTimeAdjustments);
-// view user requests
 router.get("/my-requests", authMiddleware, fetchMyTimeAdjustments);
-// Accept and decline user time adjustment requests - ADMIN only
 router.put("/:id/status", authMiddleware, updateTimeAdjustmentStatus);
-// Delete time adjustment submitted request
 router.delete("/:id", authMiddleware, deleteTimeAdjustment);
 
 export default router;
