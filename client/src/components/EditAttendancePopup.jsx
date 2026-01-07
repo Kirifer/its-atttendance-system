28ea2ba579144deed4c7bc4a5349ad1bd1d05506
Jermaine Riley M. Pasion <jmpasion@student.hau.edu.ph>
2026-01-07 11:28:38 +0800

    fix: edit attendance submission bug

--- client/src/components/EditAttendancePopup.jsx
+++ client/src/components/EditAttendancePopup.jsx
@@ -44,11 +44,19 @@ export default function EditAttendancePopup({ record, onClose, onSave }) {
         <label>Time In:</label>
         <input type="time" value={timeIn} onChange={(e) => setTimeIn(e.target.value)} />
 
-        <label>Lunch In:</label>
-        <input type="time" value={lunchIn} onChange={(e) => setLunchIn(e.target.value)} />
-
         <label>Lunch Out:</label>
-        <input type="time" value={lunchOut} onChange={(e) => setLunchOut(e.target.value)} />
+          <input
+            type="time"
+            value={lunchOut}
+            onChange={(e) => setLunchOut(e.target.value)}
+          />
+
+          <label>Lunch In:</label>
+          <input
+            type="time"
+            value={lunchIn}
+            onChange={(e) => setLunchIn(e.target.value)}
+        />
 
         <label>Time Out:</label>
         <input type="time" value={timeOut} onChange={(e) => setTimeOut(e.target.value)} />
