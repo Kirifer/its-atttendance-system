177af1b5f7263c001114ca2491fd4f03ae8c4981
C19rk <clarkmanuntag@gmail.com>
2026-01-07 16:31:25 +0800

    Updated Timeoff.jsx placeholder text into an actual text.

--- client/src/screens/Timeoff.jsx
+++ client/src/screens/Timeoff.jsx
@@ -6,11 +6,11 @@ import { createLeave } from "../api/leave";
 import Loader from "../components/Spinner/Loader";
 
 function Timeoff() {
-   const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-   const handleLeaveSubmit = async (formData) => {
+  const handleLeaveSubmit = async (formData) => {
     try {
-      setLoading(true); 
+      setLoading(true);
       await createLeave(formData);
     } catch (err) {
       console.error("Failed to create leave:", err);
@@ -20,19 +20,18 @@ function Timeoff() {
   };
 
   return (
-      <DashboardLayout>
-        <div className="timeoff__main">
-          <h2 className="timeoff_title">File a Leave</h2>
-          <p className="timeoff__description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec leo
-            diam, interdum nec placerat in, venenatis egestas justo. Nam eu
-            gravida ante, vel egestas turpis.
-          </p>
-          <Loader loading={loading}>
-            <LeaveForm onSubmit={handleLeaveSubmit} />
-          </Loader>
-        </div>
-      </DashboardLayout>
+    <DashboardLayout>
+      <div className="timeoff__main">
+        <h2 className="timeoff_title">File a Leave</h2>
+        <p className="timeoff__description">
+          Submit your leave requests here using the form below. Please ensure
+          all details are accurate before submitting.
+        </p>
+        <Loader loading={loading}>
+          <LeaveForm onSubmit={handleLeaveSubmit} />
+        </Loader>
+      </div>
+    </DashboardLayout>
   );
 }
 
