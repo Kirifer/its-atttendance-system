5c929d8d12a75ba9c10d2e9fe2d00bce7e74a127
Jermaine Riley M. Pasion <jmpasion@student.hau.edu.ph>
2026-01-12 11:16:48 +0800

    fix: firebase filepath

--- server/src/config/firebase.js
+++ server/src/config/firebase.js
@@ -12,7 +12,9 @@ const serviceAccount = JSON.parse(
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: "its-atttendance-system.firebasestorage.app",
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 });
 
-export const bucket = admin.storage().bucket();
+export const bucket = admin
+  .storage()
+  .bucket(process.env.FIREBASE_STORAGE_BUCKET);
