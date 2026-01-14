8dc6a1cedb369ecc94f7e9638b0fd97ed87436f4
donotlook <repodsalgo200zzz@gmail.com>
2026-01-14 11:23:01 +0800

    fix: typo

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ import timeAdjustmentRoutes from "./src/routes/timeAdjustmentRoutes.js";
 import adminRoutes from "./src/routes/adminRoutes.js";
 import path from "path";
 import { fileURLToPath } from "url";
-import "./src/cron/leaveChecker.js:";
-import "./src/cron/cleanExpiredSched.js"
+import "./src/cron/leaveChecker.js";
+import "./src/cron/cleanExpiredSched.js";
 
 dotenv.config();
 
