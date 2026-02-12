7c34cfa6752f64ab25d02db6fd1b60daf612c1bb
Jam Payumo <jam.payumo@users.noreply.github.com>
2026-02-12 09:18:29 +0800

    Update S3 and package

--- server/src/config/s3.js
+++ server/src/config/s3.js
@@ -11,6 +11,7 @@ if (
 
 export const s3 = new S3Client({
   region: process.env.AWS_REGION,
+  endpoint: process.env.AWS_ENDPOINT_URL,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
